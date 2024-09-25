44d452b0c91c767153838b1c44680539ea4025b3
Makerz <makerz03795@hotmail.com>
2024-09-25 17:21:50 +0700

    update: file :fire:

--- bat_file/TC_login.bat
+++ bat_file/TC_login.bat
@@ -1,3 +1,3 @@
 @echo off
-robot -T -d D:/RobotFramework/Testcase/SIT -L trace -d ../Results/TC_login/ D:/RobotFramework/Testcase/SIT/TC_login.robot
-timeout /t 5 > nul
+robot -T -d D:/RobotFramework/Testcase/SIT/ -L trace -d ../Results/TC_login/ D:/RobotFramework/Testcase/SIT/TC_login.robot
+@REM timeout /t 5 > nul
