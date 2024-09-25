3e93abd1af785abd3afbfcedc5c02ec3bbde642f
Makerz <makerz03795@hotmail.com>
2024-09-25 17:26:00 +0700

    update: file :fire:

--- bat_file/TC_login.bat
+++ bat_file/TC_login.bat
@@ -1,3 +1,13 @@
 @echo off
-robot -T -d D:/RobotFramework/Testcase/SIT/ -L trace -d ../Results/TC_login/ D:/RobotFramework/Testcase/SIT/TC_login.robot
+@REM :CheckAdmin
+@REM NET SESSION >nul 2>&1
+@REM if %errorLevel% == 0 (
+@REM     echo Running with Administrator privileges.
+@REM ) else (
+@REM     echo Requesting Administrator privileges...
+@REM     powershell Start-Process '%~0' -Verb runAs
+@REM     exit /b
+@REM )
+
+robot -T -d D:/RobotFramework/Testcase/SIT/ -i TC002 -L trace -d ../Results/TC_login/ D:/RobotFramework/Testcase/SIT/TC_login.robot
 @REM timeout /t 5 > nul
