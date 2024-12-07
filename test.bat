31b1ad985d496c9f9a7dfb1e18dcfb2093a4c08a
Makerz <makerz03795@hotmail.com>
2024-12-08 00:57:44 +0700

    update: requirment :fire:

--- test.bat
+++ test.bat
@@ -1,1 +1,3 @@
-robot -i TC001 -d C:/ProgramData/Jenkins/.jenkins/workspace/Result/Automate/TC_login C:/ProgramData/Jenkins/.jenkins/workspace/Automate/Testcase/TC.robot
+@echo off
+robot -T -i TC001 -L trace -d C:/ProgramData/Jenkins/.jenkins/workspace/Result/Automate/TC_login C:/ProgramData/Jenkins/.jenkins/workspace/Automate/Testcase/TC.robot
+timeout /t 5 > nul
