b6dca20ee905da9ab804daeb499b2f3c93ef7fad
Makerz <makerz03795@hotmail.com>
2024-12-08 00:45:39 +0700

    update: requirment :fire:

--- jenkins_batfile.bat
+++ jenkins_batfile.bat
@@ -1,11 +1,13 @@
-@echo off
-IF NOT "%1"=="null" SET tag=-i %1
-SET resultpath=%2
-SET robotpath=%3
+@REM @echo off
+@REM IF NOT "%1"=="null" SET tag=-i %1
+@REM SET resultpath=%2
+@REM SET robotpath=%3
 
 @REM echo input: %1
 @REM echo tag: %tag%   
 @REM echo resultpath: %resultpath%   
 @REM echo robotpath: %robotpath%   
-echo robot -T %tag% -L trace -d %resultpath% %robotpath%
-robot -T %tag% -L trace -d %resultpath% %robotpath%
+@REM echo robot -T %tag% -L trace -d %resultpath% %robotpath%
+@REM robot -T %tag% -L trace -d %resultpath% %robotpath%
+
+robot -T -i TC001 -L trace -d C:\ProgramData\Jenkins\.jenkins\workspace\Result\Automate\TC_login C:/ProgramData/Jenkins/.jenkins/workspace/Automate/Testcase/SIT/TC_login.robot
